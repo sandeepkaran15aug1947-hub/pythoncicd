88c14573fceffb02b46cc9b6fd1a3358ba585acb
sandeepkaran <https://github.com/sandeepNoida7>
2025-10-20 19:20:34 +0530

    jj

--- controller/EmployeeController.py
+++ controller/EmployeeController.py
@@ -1,24 +1,9 @@
 from fastapi import FastAPI
 
-obj=FastAPI()  # creating an object of FastApi
+# FastAPI instance
+obj = FastAPI()
 
-mylist=[
-    
-     { "id":101,"Name":"sandeep"},
-      { "id":101,"Name":"sandeep"}
-    ]
-
+# Example route
 @obj.get("/study")
-def mystudy():
-    return mylist
-
-@obj.get("/items")
-def read_item(name: str, price: int):
-    return {"name": name, "price": price}
-
-
-app = FastAPI()
-
-@obj.get("/items/{item_id}")
-def read_items(item_id: int):
-    return {"item_id": item_id}
+def get_study():
+    return {"msg": "Study route working"}
