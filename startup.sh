6f9da93aa2515590592aaf769fb4df4a27257717
sandeepkaran <https://github.com/sandeepNoida7>
2025-10-20 21:52:04 +0530

    jjjljj

--- startup.sh
+++ startup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+#!/bin/bash
+python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
