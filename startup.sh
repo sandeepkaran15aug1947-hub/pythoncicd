5d2759f128f028bdc534b5e2c702867fa8d67cc3
sandeepkaran <https://github.com/sandeepNoida7>
2025-10-20 23:27:34 +0530

    jjjlkjjklkkklkjj

--- startup.sh
+++ startup.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-set -e  # exit if any command fails
-
-echo "🔧 Starting FastAPI setup..."
-
-# Optional: create virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-# Upgrade pip and install dependencies
-pip install --upgrade pip
-pip install -r requirements.txt
-
-echo "🚀 Launching FastAPI with Uvicorn..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+#!/bin/bashset -e  # exit if any command failsecho "🔧 Starting FastAPI setup..."# Optional: create virtual environmentpython3 -m venv venvsource venv/bin/activate# Upgrade pip and install dependenciespip install --upgrade pippip install -r requirements.txtecho "🚀 Launching FastAPI with Uvicorn..."exec uvicorn app.main:app --host 0.0.0.0 --port 8000
